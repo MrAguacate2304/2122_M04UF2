98aacba415be41b5c6ef414e9417a71d475c2233
enti <enti@enti.enti>
2022-02-25 14:27:15 +0100

    Ejemplo de react

---
+++ ejemplo_rect/src/Piesito.js
@@ -0,0 +1,14 @@
+import './Piesito.css';
+function Piesito ()
+{
+
+	return (
+		<footer>
+			<p>Copy(c)2022 La Mama de la mama de la mama de Piesito</p>
+		</footer>
+	);
+
+}
+
+
+export default Piesito;
---
+++ ejemplo_rect/src/Titulito.js
@@ -0,0 +1,16 @@
+
+
+
+function Titulito ()
+{
+	return (
+		<div id="titulito">
+		<h1>Marachullao</h1>
+		<p><strong><em>El mejor titulo de todos</em></strong></p>
+		</div>
+	);
+
+}
+
+
+export default Titulito;
